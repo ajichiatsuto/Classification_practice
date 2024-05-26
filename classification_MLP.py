eebb9e6fb4fd367d56c5f92aa83aea14341cef34
ajichiatsuto <s2410004@jaist.ac.jp>
2024-05-26 16:39:57 +0900

    add normalization

--- classification_MLP.py
+++ classification_MLP.py
@@ -1,15 +1,36 @@
 from sklearn.neural_network import MLPClassifier
 from sklearn.datasets import make_moons
+from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 import mglearn as mg
 import matplotlib.pyplot as plt
 
-X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
-X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
+# X, y = make_moons(n_samples=100, noise=0.25, random_state=3)
+# X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y, random_state=42)
 
-mlp = MLPClassifier(solver='lbfgs', activation="tanh", random_state=0, hidden_layer_sizes=[10, 10]).fit(X_train, y_train)
-mg.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3)
-mg.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
-plt.xlabel("Feature 0")
-plt.ylabel("Feature 1")
+# mlp = MLPClassifier(solver='lbfgs', activation="tanh", random_state=0, hidden_layer_sizes=[10, 10]).fit(X_train, y_train)
+# mg.plots.plot_2d_separator(mlp, X_train, fill=True, alpha=.3)
+# mg.discrete_scatter(X_train[:, 0], X_train[:, 1], y_train)
+# plt.xlabel("Feature 0")
+# plt.ylabel("Feature 1")
+# plt.show()
+
+cancer = load_breast_cancer()
+X_train, X_test, y_train, y_test = train_test_split(
+    cancer.data, cancer.target, stratify=cancer.target, random_state=0)
+
+mean_on_train = X_train.mean(axis=0)
+std_on_train = X_train.std(axis=0)
+X_train_scaled = (X_train - mean_on_train) / std_on_train
+X_test_scaled = (X_test - mean_on_train) / std_on_train
+mlp = MLPClassifier(random_state=42).fit(X_train_scaled, y_train)
+print("Accuracy on training set: {:.2f}".format(mlp.score(X_train_scaled, y_train)))
+print("Accuracy on test set: {:.2f}".format(mlp.score(X_test_scaled, y_test)))
+
+plt.figure(figsize=(20, 5))
+plt.imshow(mlp.coefs_[0], interpolation='none', cmap='viridis')
+plt.yticks(range(30), cancer.feature_names)
+plt.xlabel("Columns in weight matrix")
+plt.ylabel("Input feature")
+plt.colorbar()
 plt.show()
